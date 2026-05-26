17e32ad9001565db53134dc74ea88a87fe7c1673
funnyboy-roks <fbr@fbr.dev>
2026-05-26 02:28:46 -0500

    slightly tidy main

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,26 @@
-use std::{cell::RefCell, collections::BTreeMap, fmt::Display, io::Write, rc::Rc, time::Instant};
+use std::{cell::RefCell, collections::BTreeMap, rc::Rc, time::Instant};
 
 use anstream::{eprintln, println};
 use chrono::Utc;
 use clap::Parser;
 use miette::{Context, IntoDiagnostic, bail};
 use reqwest::blocking::Client;
-use serde_json::Value as JsonValue;
 
 use crate::{
     cli::{Cli, QueryCommand, SubCmd, VariableCommand},
     config::Config,
-    script::{ScriptBody, ScriptResponse, script_engine},
+    print::{print_request, print_response, print_variable},
+    script::{ScriptResponse, script_engine},
     state::State,
 };
 
 mod cli;
 mod config;
 mod parse;
+mod print;
 mod script;
 mod state;
 mod util;
-
-fn pretty_print_json(w: &mut impl Write, json: JsonValue, indent: usize) -> std::io::Result<()> {
-    use owo_colors::OwoColorize as _;
-
-    fn apply_indent(w: &mut impl Write, indent: usize) -> std::io::Result<()> {
-        write!(w, "\n{0:>1$}", "", indent * 4)
-    }
-
-    match json {
-        JsonValue::Null => write!(w, "{}", "null".bright_black()),
-        JsonValue::Bool(b) => write!(w, "{}", b.cyan()),
-        JsonValue::Number(number) => write!(w, "{}", number.yellow()),
-        JsonValue::String(_) => write!(w, "{}", json.green()),
-        JsonValue::Array(values) => {
-            write!(w, "[")?;
-            if !values.is_empty() {
-                for (i, v) in values.into_iter().enumerate() {
-                    if i > 0 {
-                        write!(w, ",")?;
-                    }
-                    apply_indent(w, indent + 1)?;
-                    pretty_print_json(w, v, indent + 1)?;
-                }
-                apply_indent(w, indent)?;
-            }
-            write!(w, "]")
-        }
-        JsonValue::Object(map) => {
-            write!(w, "{{")?;
-            if !map.is_empty() {
-                for (i, (k, v)) in map.into_iter().enumerate() {
-                    if i > 0 {
-                        write!(w, ",")?;
-                    }
-                    apply_indent(w, indent + 1)?;
-                    write!(w, "{}: ", JsonValue::String(k).blue().bold())?;
-                    pretty_print_json(w, v, indent + 1)?;
-                }
-                apply_indent(w, indent)?;
-            }
-            write!(w, "}}")
-        }
-    }
-}
 
 fn run_query(
     cli: &Cli,
@@ -97,107 +54,14 @@ fn run_query(
 
     let (req, req_body) = query.to_request(&client, &vars)?;
 
-    {
-        use owo_colors::OwoColorize as _;
-
-        eprintln!("{}:", "Request Details".cyan());
-
-        eprintln!(
-            "  {}:   {:?}",
-            "HTTP Version".blue(),
-            req.version().yellow()
-        );
-        eprintln!("  {}:         {}", "Method".blue(), req.method().yellow());
-        eprintln!("  {}:            {}", "URL".blue(), req.url().yellow());
-        if let Some(timeout) = req.timeout() {
-            eprintln!("  {}:            {:?}", "Timeout".blue(), timeout.yellow());
-        }
-
-        eprintln!("  {}:", "Headers".blue());
-        for (name, value) in req.headers() {
-            match value.to_str() {
-                Ok(s) => eprintln!("    {}: {}", name.yellow(), s),
-                Err(_) => eprintln!("    {}: {:?}", name.yellow(), value),
-            }
-        }
-
-        if let Some(req_body) = req_body {
-            match req_body {
-                config::PopulatedBody::Json(value) => {
-                    eprintln!("{}:", "Request Body (JSON)".cyan());
-                    pretty_print_json(&mut anstream::stderr().lock(), value, 0)
-                        .into_diagnostic()?;
-                    eprintln!();
-                }
-                config::PopulatedBody::Text(cow) => {
-                    eprintln!("{}:", "Request Body (Raw)".cyan());
-
-                    eprintln!("{}", cow);
-                }
-            }
-        }
-
-        eprintln!("{}", "Sending Request...".purple());
-    }
+    print_request(&req, req_body)?;
 
     let start = Instant::now();
     let res = client.execute(req).into_diagnostic()?;
     let res = ScriptResponse::from_response(res, query_cmd.raw)?;
     let elapsed = start.elapsed();
 
-    {
-        use owo_colors::OwoColorize as _;
-
-        eprintln!("{}:", "Response Details".cyan());
-        let status = res.status();
-        let status: &dyn Display = if status.is_server_error() {
-            &status.bright_red()
-        } else if status.is_client_error() {
-            &status.red()
-        } else if status.is_informational() {
-            &status.blue()
-        } else if status.is_redirection() {
-            &status.yellow()
-        } else if status.is_success() {
-            &status.green()
-        } else {
-            &status
-        };
-        eprintln!("  {}:   {:?}", "HTTP Version".blue(), res.version.yellow());
-        eprintln!("  {}:            {}", "URL".blue(), res.url.yellow());
-        eprintln!("  {}:       {:?}", "Duration".blue(), elapsed.yellow());
-        eprintln!("  {}:    {}", "Status Code".blue(), status);
-        if let Some(remote_addr) = res.remote_addr {
-            eprintln!("  {}: {}", "Remote Address".blue(), remote_addr.yellow());
-        }
-
-        eprintln!("  {}:", "Headers".blue());
-        for (name, value) in &*res.headers {
-            match value.to_str() {
-                Ok(s) => eprintln!("    {}: {}", name.yellow(), s),
-                Err(_) => eprintln!("    {}: {:?}", name.yellow(), value),
-            }
-        }
-
-        if let Some(content_len) = res.content_length
-            && content_len != 0
-        {
-            match &res.body {
-                ScriptBody::Text(t) => {
-                    eprintln!("{}:", "Response Body (Raw)".cyan());
-                    std::io::stdout()
-                        .write_all(t.as_bytes())
-                        .into_diagnostic()?;
-                }
-                ScriptBody::Json(json) => {
-                    eprintln!("{}:", "Response Body (JSON)".cyan());
-                    pretty_print_json(&mut anstream::stdout().lock(), json.clone(), 0)
-                        .into_diagnostic()?;
-                    println!();
-                }
-            }
-        }
-    }
+    print_response(&res, elapsed)?;
 
     if let Some(post_script) = query.post_script {
         eprintln!(
@@ -252,26 +116,7 @@ fn run_variable(
         },
         cli::VariableSubCmd::Get { variable } => match state.borrow().variables.get(variable) {
             Some(v) => {
-                {
-                    use owo_colors::OwoColorize as _;
-                    eprint!("{}   ", "Value:".blue());
-                    let _ = std::io::stderr().flush(); // ensure the Value: is printed
-
-                    println!("{}", v.value); // print to stdout so it can be piped
-
-                    if let Some(expires_at) = v.expires_at {
-                        eprintln!(
-                            "{} {} {}",
-                            "Expires:".blue(),
-                            expires_at
-                                .with_timezone(&chrono::Local)
-                                .format("%Y-%m-%d %H:%M:%S"),
-                            format!("({})", chrono_humanize::HumanTime::from(expires_at)).yellow(),
-                        );
-                    } else {
-                        eprintln!("{} Never", "Expires:".blue());
-                    }
-                }
+                print_variable(v, false);
             }
             None => {
                 use owo_colors::OwoColorize as _;
@@ -283,26 +128,8 @@ fn run_variable(
             let variables = &state.borrow().variables;
             let variables = BTreeMap::from_iter(variables);
             for (variable, v) in variables {
-                {
-                    use owo_colors::OwoColorize as _;
-
-                    println!("{}", variable.green());
-
-                    println!("  {}   {}", "Value:".blue(), v.value);
-
-                    if let Some(expires_at) = v.expires_at {
-                        println!(
-                            "  {} {} {}",
-                            "Expires:".blue(),
-                            expires_at
-                                .with_timezone(&chrono::Local)
-                                .format("%Y-%m-%d %H:%M:%S"),
-                            format!("({})", chrono_humanize::HumanTime::from(expires_at)).yellow(),
-                        );
-                    } else {
-                        println!("  {} Never", "Expires:".blue());
-                    }
-                }
+                println!("{}", owo_colors::OwoColorize::green(&variable));
+                print_variable(v, true);
             }
         }
     }
---
+++ src/print.rs
@@ -0,0 +1,183 @@
+use std::{fmt::Display, io::Write, time::Duration};
+
+use miette::IntoDiagnostic;
+use reqwest::blocking::Request;
+use serde_json::Value as JsonValue;
+
+use crate::{
+    config::{self, PopulatedBody},
+    script::{ScriptBody, ScriptResponse},
+    state::PersistedVariable,
+    util::DATETIME_FORMAT,
+};
+
+fn pretty_print_json(w: &mut impl Write, json: JsonValue, indent: usize) -> std::io::Result<()> {
+    use owo_colors::OwoColorize as _;
+
+    fn apply_indent(w: &mut impl Write, indent: usize) -> std::io::Result<()> {
+        write!(w, "\n{0:>1$}", "", indent * 4)
+    }
+
+    match json {
+        JsonValue::Null => write!(w, "{}", "null".bright_black()),
+        JsonValue::Bool(b) => write!(w, "{}", b.cyan()),
+        JsonValue::Number(number) => write!(w, "{}", number.yellow()),
+        JsonValue::String(_) => write!(w, "{}", json.green()),
+        JsonValue::Array(values) => {
+            write!(w, "[")?;
+            if !values.is_empty() {
+                for (i, v) in values.into_iter().enumerate() {
+                    if i > 0 {
+                        write!(w, ",")?;
+                    }
+                    apply_indent(w, indent + 1)?;
+                    pretty_print_json(w, v, indent + 1)?;
+                }
+                apply_indent(w, indent)?;
+            }
+            write!(w, "]")
+        }
+        JsonValue::Object(map) => {
+            write!(w, "{{")?;
+            if !map.is_empty() {
+                for (i, (k, v)) in map.into_iter().enumerate() {
+                    if i > 0 {
+                        write!(w, ",")?;
+                    }
+                    apply_indent(w, indent + 1)?;
+                    write!(w, "{}: ", JsonValue::String(k).blue().bold())?;
+                    pretty_print_json(w, v, indent + 1)?;
+                }
+                apply_indent(w, indent)?;
+            }
+            write!(w, "}}")
+        }
+    }
+}
+
+pub fn print_request(req: &Request, body: Option<PopulatedBody<'_>>) -> miette::Result<()> {
+    use owo_colors::OwoColorize as _;
+
+    eprintln!("{}:", "Request Details".cyan());
+
+    eprintln!(
+        "  {}:   {:?}",
+        "HTTP Version".blue(),
+        req.version().yellow()
+    );
+    eprintln!("  {}:         {}", "Method".blue(), req.method().yellow());
+    eprintln!("  {}:            {}", "URL".blue(), req.url().yellow());
+    if let Some(timeout) = req.timeout() {
+        eprintln!("  {}:            {:?}", "Timeout".blue(), timeout.yellow());
+    }
+
+    eprintln!("  {}:", "Headers".blue());
+    for (name, value) in req.headers() {
+        match value.to_str() {
+            Ok(s) => eprintln!("    {}: {}", name.yellow(), s),
+            Err(_) => eprintln!("    {}: {:?}", name.yellow(), value),
+        }
+    }
+
+    if let Some(req_body) = body {
+        match req_body {
+            config::PopulatedBody::Json(value) => {
+                eprintln!("{}:", "Request Body (JSON)".cyan());
+                pretty_print_json(&mut anstream::stderr().lock(), value, 0).into_diagnostic()?;
+                eprintln!();
+            }
+            config::PopulatedBody::Text(cow) => {
+                eprintln!("{}:", "Request Body (Raw)".cyan());
+
+                eprintln!("{}", cow);
+            }
+        }
+    }
+
+    eprintln!("{}", "Sending Request...".purple());
+
+    Ok(())
+}
+
+pub fn print_response(res: &ScriptResponse, elapsed: Duration) -> miette::Result<()> {
+    use owo_colors::OwoColorize as _;
+
+    eprintln!("{}:", "Response Details".cyan());
+    let status = res.status();
+    let status: &dyn Display = if status.is_server_error() {
+        &status.bright_red()
+    } else if status.is_client_error() {
+        &status.red()
+    } else if status.is_informational() {
+        &status.blue()
+    } else if status.is_redirection() {
+        &status.yellow()
+    } else if status.is_success() {
+        &status.green()
+    } else {
+        &status
+    };
+    eprintln!("  {}:   {:?}", "HTTP Version".blue(), res.version.yellow());
+    eprintln!("  {}:            {}", "URL".blue(), res.url.yellow());
+    eprintln!("  {}:       {:?}", "Duration".blue(), elapsed.yellow());
+    eprintln!("  {}:    {}", "Status Code".blue(), status);
+    if let Some(remote_addr) = res.remote_addr {
+        eprintln!("  {}: {}", "Remote Address".blue(), remote_addr.yellow());
+    }
+
+    eprintln!("  {}:", "Headers".blue());
+    for (name, value) in &*res.headers {
+        match value.to_str() {
+            Ok(s) => eprintln!("    {}: {}", name.yellow(), s),
+            Err(_) => eprintln!("    {}: {:?}", name.yellow(), value),
+        }
+    }
+
+    if let Some(content_len) = res.content_length
+        && content_len != 0
+    {
+        match &res.body {
+            ScriptBody::Text(t) => {
+                eprintln!("{}:", "Response Body (Raw)".cyan());
+                std::io::stdout()
+                    .write_all(t.as_bytes())
+                    .into_diagnostic()?;
+            }
+            ScriptBody::Json(json) => {
+                eprintln!("{}:", "Response Body (JSON)".cyan());
+                pretty_print_json(&mut anstream::stdout().lock(), json.clone(), 0)
+                    .into_diagnostic()?;
+                println!();
+            }
+        }
+    }
+
+    Ok(())
+}
+
+pub fn print_variable(v: &PersistedVariable, indent: bool) {
+    use owo_colors::OwoColorize as _;
+    if indent {
+        eprint!("  ");
+    }
+    eprint!("{}   ", "Value:".blue());
+    let _ = std::io::stderr().flush(); // ensure the Value: is printed
+
+    println!("{}", v.value); // print to stdout so it can be piped
+
+    if let Some(expires_at) = v.expires_at {
+        if indent {
+            eprint!("  ");
+        }
+        eprintln!(
+            "{} {} {}",
+            "Expires:".blue(),
+            expires_at
+                .with_timezone(&chrono::Local)
+                .format(DATETIME_FORMAT),
+            format!("({})", chrono_humanize::HumanTime::from(expires_at)).yellow(),
+        );
+    } else {
+        eprintln!("{} Never", "Expires:".blue());
+    }
+}
